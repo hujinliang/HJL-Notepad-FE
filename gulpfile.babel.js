e172412010dbbefb2e2d186a3fef2178165283f6
hujingliang <1617451312@qq.com>
2016-09-27 12:31:49 +0800

    '生产环境配置'

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,8 @@ import gutil from 'gulp-util'
 import WebpackDevServer from 'webpack-dev-server'
 import webpack from 'webpack'
 import open from 'open'
+import env from 'gulp-env'
+import gulpSequence from 'gulp-sequence'
 
 const DEV_PORT =3000,PROD_PORT =9400;
 gulp.task('serve',function(){
@@ -28,4 +30,22 @@ gulp.task('serve',function(){
         gutil.log("[webpack-dev-server]", "==> 🌎  http://localhost:" + DEV_PORT)
         // open('http://localhost:' + DEV_PORT)
     })
+});
+
+gulp.task('set-env-prod',() => {
+    env({
+        vars:{
+            'NODE_ENV':'production'
+        }
+    })
 })
+
+gulp.task('webpack',cb => {
+    let webpackConfig = require('./webpack.config');
+    let myConfig = Object.create(webpackConfig);
+    webpack(myConfig,function(err,status){
+        cb()
+    })
+})
+
+gulp.task('build',gulpSequence('set-env-prod','webpack'))
